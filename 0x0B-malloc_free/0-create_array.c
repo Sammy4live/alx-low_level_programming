df9530917dbca2ab6ff46ea8be56e6026381817a
Samson <samuelohida2022@gmail.com>
2023-03-20 04:19:07 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,20 +12,13 @@ char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
 
-	char *s;
+	char *str;
 
-	if (size == 0)
+	str = malloc(sizeof (char) * size);
+	if (size == 0 || str == NULL)
 		return (NULL);
-	s = malloc(size * sizeof(char));
-
-	if (s == NULL)
-
-	return (NULL);
 
 	for (i = 0; i < size; i++)
-
-{
-	s[i] = c;
-	}
-	return (s);
+		str[i] = c;
+	return (str);
 }
